91c97c24bbaaa1f68ea630499d1f4153e9039af7
maximpanov <maksim.panov@timebook.ru>
2020-11-13 17:15:55 +0300

    #3 - AuthResponse wrap in lombok

--- src/main/java/ru/localcat/orion2020todo/responses/AuthResponse.java
+++ src/main/java/ru/localcat/orion2020todo/responses/AuthResponse.java
@@ -1,19 +1,14 @@
 package ru.localcat.orion2020todo.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
-    private final String token;
-    private final Integer code;
-
-    public AuthResponse(String status, Integer code) {
-        this.token = status;
-        this.code = code;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
+    @Getter
+    private String token;
+    @Getter
+    private Integer code;
 }
