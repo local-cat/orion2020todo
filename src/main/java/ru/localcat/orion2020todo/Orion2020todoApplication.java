7f5ffc87a08af1891c620404588dae87553f94c6
maximpanov <maksim.panov@timebook.ru>
2020-11-13 13:30:09 +0300

    #1 - init commit

---
+++ src/main/java/ru/localcat/orion2020todo/Orion2020todoApplication.java
@@ -0,0 +1,13 @@
+package ru.localcat.orion2020todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Orion2020todoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Orion2020todoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/localcat/orion2020todo/Orion2020todoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.localcat.orion2020todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Orion2020todoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
